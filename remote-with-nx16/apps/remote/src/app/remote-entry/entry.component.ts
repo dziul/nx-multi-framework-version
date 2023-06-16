dbd00e3df9cdaed4097b82fa8f9b6006e411b0c7
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2023-06-16 05:20:13 +0000

    feat: add apps remote and shell

---
+++ remote-with-nx16/apps/remote/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'remote-with-nx16-remote-entry',
+  template: `<remote-with-nx16-nx-welcome></remote-with-nx16-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ shell-with-nx12/apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shell-with-nx12-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
